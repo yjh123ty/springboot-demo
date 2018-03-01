27bb6021ed719c3fcd7530dd6dd184995fa34aed
jiahao.yu <245372221@qq.com>
2018-03-01 10:48:33 +0800

    修改为打jar包

--- src/main/java/com/yoga/demo/DemoApplication.java
+++ src/main/java/com/yoga/demo/DemoApplication.java
@@ -3,8 +3,6 @@ package com.yoga.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -23,16 +21,11 @@ import com.yoga.demo.utils.wechat.SingleAccessToken;
 //@EnableScheduling				//在启动类上面加上@EnableScheduling即可开启定时。
 @EnableTransactionManagement
 @ImportResource(locations = {"classpath:spring/spring-context.xml"})
-public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.err.println(SingleAccessToken.getInstance().getAccessToken());
 	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
-    }
 }
 
