861a88b9092e471666eea8ac1224991ec8b1c02e
jiahao.yu <245372221@qq.com>
2018-03-08 12:57:16 +0800

    环境切换配置

--- src/main/java/com/yoga/demo/controller/MainController.java
+++ src/main/java/com/yoga/demo/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.yoga.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-   
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Value("${show.env}")
+	private String env;
+	
 	@RequestMapping(value = "/",method = RequestMethod.GET)
     public String main(Model model){
+		log.info("当前的系统环境为{}.", env);
         return "pages/main/index";
     }
 	
 	@RequestMapping(value ="/main/index",method = RequestMethod.GET)
     public String mainIndex(Model model){
+		log.info("当前的系统环境为{}.", env);
         return "pages/main/main_info";
     }
 
--- src/main/java/com/yoga/demo/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/yoga/demo/service/impl/UserInfoServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
