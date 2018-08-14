1e95aca9393f0a6bd9ca2d5e76f1acb900e53e10
yjh123yu <245372221@qq.com>
2018-08-14 23:53:45 +0800

    commit

---
+++ src/main/java/com/yoga/demo/utils/config/Config.java
@@ -0,0 +1,13 @@
+package com.yoga.demo.utils.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "springdemo")
+public class Config {
+
+
+    public class Cfg{
+
+    }
+
+}
