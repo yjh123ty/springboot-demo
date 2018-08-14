34ce8480bbd6003501326e43390e4f4962d1a7a9
yjh123yu <245372221@qq.com>
2018-08-14 23:59:55 +0800

    修改代码

--- src/main/java/com/yoga/demo/utils/config/Config.java
+++ src/main/java/com/yoga/demo/utils/config/Config.java
@@ -1,12 +1,21 @@
 package com.yoga.demo.utils.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "springdemo")
+@Configuration
 public class Config {
 
+    @Data
+    public static class Cfg{
+        private String var1;
+        private String var2;
+        private String var3;
+    }
 
-    public class Cfg{
+    public static void main(String[] args) {
 
     }
 
